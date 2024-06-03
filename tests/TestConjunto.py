06a6c8daf476cde24884bb0a7ce7b1f2f8a81664
75411510 <75411510@continental.edu.pe>
2024-06-03 13:05:54 -0500

    cambio promedio ponderado (verde)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -18,6 +18,19 @@ class TestConjunto(unittest.TestCase):
         conjunto=Conjunto([2,4,8,9,10,15])
         self.assertEqual((2+4+8+9+10+15)/6,conjunto.promedio())
 
+## promedio ponderado
+    class TestConjuntoPonderado(unittest.TestCase):
+        def test_promedio_ponderado_caso1(self):
+            datos = [10, 12, 14]
+            pesos = [3, 4, 2]
+            conjunto_ponderado = ConjuntoPonderado(datos, pesos)
+            self.assertAlmostEqual(11.78, conjunto_ponderado.promedio_ponderado(), delta=0.01)
+
+        def test_promedio_ponderado_caso2(self):
+            datos = [15, 15, 17]
+            pesos = [3, 4, 2]
+            conjunto_ponderado = ConjuntoPonderado(datos, pesos)
+            self.assertAlmostEqual(15.44, conjunto_ponderado.promedio_ponderado(), delta=0.01)
 
 if __name__ == '__main__':
     unittest.main()
